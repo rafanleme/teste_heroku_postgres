4efeb3e9d9c1ee450e2b4fb2b9fce196d69c0672
rafanleme <rafanleme@gmail.com>
2020-11-27 17:51:34 -0300

    commit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,19 +4,21 @@ var admin = require("firebase-admin");
 
 const BUCKET = "senai-overflow2.appspot.com";
 
+console.log(process.env.FIREBASE_PRIVATE_KEY);
+
 admin.initializeApp({
   credential: admin.credential.cert({
     type: "service_account",
     project_id: "senai-overflow",
     private_key_id: "73066f27e33601b0035117947f795a484b773efe",
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: "105469033790114328493",
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     client_x509_cert_url:
       "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-k09tb%40senai-overflow.iam.gserviceaccount.com",
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
   storageBucket: BUCKET,
 });
